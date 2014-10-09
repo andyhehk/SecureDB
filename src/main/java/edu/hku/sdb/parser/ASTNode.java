996625ba59451f38da8e5b01b76cd1506c47432d
Haibin Lin <linhaibin.eric@gmail.com>
2014-10-09 18:16:40 +0800

    Printpretty for AST node: Create table & one-level subquery

--- src/main/java/edu/hku/sdb/parser/ASTNode.java
+++ src/main/java/edu/hku/sdb/parser/ASTNode.java
@@ -119,5 +119,173 @@ public class ASTNode extends CommonTree implements Node,Serializable {
     sb.append(')');
     return sb.toString();
   }
+    @Override
+    public String toStringTree() {
+        if ( children==null || children.size()==0 ) {
+            return this.toString();
+        }
+        StringBuffer buf = new StringBuffer();
+        if ( !isNil() ) {
+            if (token.getText().equals("TOK_QUERY")){
+                ASTNode insertTree = (ASTNode)children.get(findChild("TOK_INSERT"));
+                int groupByIndex = insertTree.findChild("TOK_GROUPBY");
+                if (groupByIndex != 0){
+                    ASTNode groupByTree = (ASTNode) insertTree.getChildren().get(groupByIndex);
+                    StringBuffer groupByBuffer = new StringBuffer();
+                    groupByBuffer.append(groupByTree.toStringTree());
+                    Tree second = (Tree)children.get(1);
+                    buf.append(second.toStringTree());
+                    buf.delete(buf.length()-groupByBuffer.length(), buf.length());
+                    Tree first = (Tree)children.get(0);
+                    buf.append(first.toStringTree());
+                    buf.append(" ");
+                    buf.append(groupByBuffer.toString());
+                    buf.append(" ");
+                    return buf.toString();
+                }
+                else {
+                    Tree second = (Tree)children.get(1);
+                    buf.append(second.toStringTree());
+                    Tree first = (Tree)children.get(0);
+                    buf.append(first.toStringTree());
+                    return buf.toString();
+                }
+            }
+            
+            else if (token.getText().equals("TOK_SUBQUERY")){
+                buf.append("(");
+                Tree first = (Tree)children.get(0);
+                buf.append(first.toStringTree());
+                buf.append(")");
+                buf.append(" ");
+                Tree second = (Tree)children.get(1);
+                buf.append(second.toStringTree());
+                buf.append(" ");
+                return buf.toString();
+            }
+            
+            else if (token.getText().equals("TOK_DESTINATION")){
+                return "";
+            }
+            
+            
+            else if (token.getText().equals("TOK_SELECT")){
+                buf.append(this.toString());
+                childToStringTree(buf);
+                buf.deleteCharAt(buf.indexOf(","));
+                buf.append(" ");
+                return buf.toString();
+                
+            }
+            else if (token.getText().equals("TOK_TABCOLLIST")){
+                buf.append("(");
+                childToStringTree(buf);
+                buf.deleteCharAt(buf.length()-1);
+                buf.append(")");
+                buf.append(" ");
+                return buf.toString();
+            }
+            else if (token.getText().equals("TOK_TABCOL")){
+                childToStringTree(buf);
+                buf.append(",");
+                return buf.toString();
+            }
+            else if (token.getText().equals("*")){
+                Tree first = (Tree)children.get(0);
+                buf.append(first.toStringTree());
+                buf.append(" * ");
+                Tree second = (Tree)children.get(1);
+                buf.append(second.toStringTree());
+                buf.append(" ");
+                return buf.toString();
+            }
+            else if (token.getText().equals("TOK_JOIN")){
+                Tree first = (Tree)children.get(0);
+                buf.append(first.toStringTree());
+                buf.append(" JOIN ");
+                Tree second = (Tree)children.get(1);
+                buf.append(second.toStringTree());
+                buf.append(" ");
+                return buf.toString();
+            }
+            
+            if (token.getText().equals("TOK_FUNCTION")){
+                if (getChildCount() > 1){
+                    ASTNode functionTree = (ASTNode) children.get(0);
+                    buf.append(functionTree.toString());
+                    buf.append("(");
+                    ASTNode paramsTree = (ASTNode) children.get(1);
+                    buf.append(paramsTree.toStringTree());
+                    buf.append(")");
+                    return buf.toString();
+                }
+                else
+                    return buf.toString();
+            }
+            else {
+                buf.append(this.toString());
+                buf.append(' ');
+            }
+        }
+        childToStringTree(buf);
+        return buf.toString();
+    }
+    private void childToStringTree(StringBuffer buf){
+        for (int i = 0; children!=null && i < children.size(); i++) {
+            Tree t = (Tree)children.get(i);
+            if ( i>0 ) {
+                buf.append(' ');
+            }
+            buf.append(t.toStringTree());
+        }
+    }
+    
+    private int findChild(String token){
+        for (int i = 0; children!=null && i < children.size(); i++) {
+            Tree t = (Tree)children.get(i);
+            if (t.getText().equals(token))   return i;
+        }
+        return 0;
+    }
+    
+    public String toString() {
+        if ( isNil() )
+            return "nil";
+        if ( getType()==Token.INVALID_TOKEN_TYPE )
+            return "<errornode>";
+        if ( token==null )
+            return null;
+        else if (token.getText().equals("TOK_CREATETABLE"))
+            return "CREATE TABLE";
+        else if (token.getText().equals("TOK_SELEXPR"))
+            return ",";
+        else if (token.getText().equals("TOK_TABNAME"))
+            return "";
+        else if (token.getText().equals("TOK_FROM"))
+            return "FROM";
+        else if (token.getText().equals("TOK_SELECT"))
+            return "SELECT";
+        else if (token.getText().equals("TOK_FUNCTION"))
+            return "";
+        else if (token.getText().equals("TOK_INT"))
+            return "int";
+        else if (token.getText().equals("TOK_ENC"))
+            return "ENC";
+        else if (token.getText().equals("<EOF>"))
+            return ";";
+        else if (token.getText().equals("TOK_TABREF"))
+            return "";
+        else if (token.getText().equals("TOK_TABCOLLIST"))
+            return "";
+        else if (token.getText().equals("TOK_INSERT"))
+            return "";
+        else if (token.getText().equals("TOK_TABLE_OR_COL"))
+            return "";
+        else if (token.getText().equals("TOK_LIKETABLE"))
+            return "";
+        else if (token.getText().equals("TOK_GROUPBY"))
+            return "GROUP BY";
+        else return token.getText();
+    }
 
 }
--- src/test/java/edu/hku/sdb/parser/AppTest.java
+++ src/test/java/edu/hku/sdb/parser/AppTest.java
@@ -3,6 +3,9 @@ package edu.hku.sdb.parser;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.antlr.runtime.tree.*;
+import org.antlr.stringtemplate.*;
+
 
 /**
  * Unit test for simple App.
@@ -32,8 +35,44 @@ public class AppTest
      * Rigourous Test :-)
      */
     public void testApp()
+    
     {
+        ParseDriver pd = new ParseDriver();
+        //        String stmtArray = "  SELECT COUNT(*) FROM wgEncodeBroadHistone.wgEncodeBroadHistoneGm12878ControlStdSig  ; SELECT COUNT(*" +
+        //                ") FROM wgEncodeBroadHistone.wgEncodeBroadHistoneGm12878ControlStdSig  ;";
+        String stmtArray = "CREATE TABLE employee (\n" +
+        "id int ,\n" +
+        "age int,\n" +
+        "salary int ENC  \n" +
+        ") ;"
         
+        
+        //              + "SELECT page_views.*\n" +
+        //              "FROM page_views JOIN dim_users\n" +
+        //              "  ON (page_views.user_id = dim_users.id " +
+        //              "AND page_views.date >= '2008-03-01' AND page_views.date <= '2008-03-31')\n"
+        
+        + "select id, sum(price*quantity) "
+        +"from (select id, price, quantity from A JOIN B ) t "
+        +"group by id;";
+        
+        
+        //SELECT col1 FROM (SELECT col1, SUM(col2) AS col2sum FROM t1 GROUP BY col1) t2 WHERE t2.col2sum > 10
+        ;
+        
+        
+        String[] stmts = stmtArray.split(";");
+        for (String stmt : stmts)
+            try {
+                ASTNode tree = pd.parse(stmt.trim());
+                DOTTreeGenerator gen = new DOTTreeGenerator();
+                StringTemplate st = gen.toDOT(tree);
+                System.out.println(st);
+                System.out.println(stmt);
+                System.out.println(tree.toStringTree());
+            } catch (ParseException e) {
+                System.out.println(e.getMessage());
+            }
         assertTrue( true );
     }
 }
