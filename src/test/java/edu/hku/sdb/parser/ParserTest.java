c42a420355f08a9e9c8c0a508659a15ca3b7b3c7
Haibin Lin <linhaibin.eric@gmail.com>
2014-10-19 20:49:02 +0800

    + Add TestUtility.

---
+++ src/test/java/edu/hku/sdb/parser/ParserTest.java
@@ -0,0 +1,84 @@
+package edu.hku.sdb.parser;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.antlr.runtime.tree.*;
+import org.antlr.stringtemplate.*;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class ParserTest extends TestCase
+
+{
+
+    private ParseDriver parseDriver;
+    private String stmtArraySimple;
+    private String stmtArrayOneLevel;
+
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public ParserTest ( String testName )
+    {
+        super( testName );
+    }
+
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( ParserTest.class );
+    }
+
+    public void setUp(){
+        this.parseDriver = new ParseDriver();
+
+        this.stmtArraySimple = "CREATE TABLE employee (id int, age int, salary int ENC);";
+        this.stmtArrayOneLevel = "select id, sum(price*quantity) from (select id, price, quantity from A JOIN B ) t group by id;";
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testParserSimple()
+    {
+        String[] stmts = stmtArraySimple.split(";");
+        for (String stmt : stmts)
+            try {
+                doPrettyPrint(stmt);
+            } catch (ParseException e) {
+                System.out.println(e.getMessage());
+            }
+        assertTrue( true );
+    }
+
+    public void testParserOneLevel(){
+        String[] stmts = stmtArrayOneLevel.split(";");
+        for (String stmt : stmts)
+            try {
+                doPrettyPrint(stmt);
+            } catch (ParseException e) {
+                System.out.println(e.getMessage());
+            }
+        assertTrue( true );
+    }
+
+    private void printVizTree(ASTNode tree){
+        TestUtility testUtility = new TestUtility(tree);
+        System.out.println(testUtility.visualize());
+    }
+
+    private void doPrettyPrint(String stmt) throws ParseException{
+        ASTNode tree = parseDriver.parse(stmt.trim());
+        System.out.println("Input SQL: " + stmt);
+        System.out.println("Output SQL: " + tree.prettyPrint());
+    }
+
+}
