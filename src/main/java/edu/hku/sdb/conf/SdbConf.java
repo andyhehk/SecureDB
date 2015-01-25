d2d9906fe289c30f561fc97582137074dd840e80
Eric Haibin Lin <linhaibin.eric@gmail.com>
2015-01-25 11:37:19 +0800

    + get SDB_CONF from system environment.

--- src/main/java/edu/hku/sdb/conf/SdbConf.java
+++ src/main/java/edu/hku/sdb/conf/SdbConf.java
@@ -29,7 +29,14 @@ public class SdbConf {
     private DbConf metaDbConf;
     private DbConf serverDbConf;
     private DbConf clientDbConf;
-    private String sdbPath;
+    private String sdbConfPath;
+
+    private static final String CLIENT_CONF_FILE = "sdb-client.xml";
+    private static final String SERVER_CONF_FILE = "sdb-server.xml";
+    private static final String METASTORE_CONF_FILE = "sdb-metastore.xml";
+    private static final String CONNECTION_CONF_FILE = "sdb-connection.xml";
+    private static final String SDB_CONF = "SDB_CONF";
+
 
     public DbConf getMetadbConf() {
         return metaDbConf;
@@ -63,12 +70,20 @@ public class SdbConf {
         this.connectionConf = connectionConf;
     }
 
+    public String getSdbConfPath() {
+        return sdbConfPath;
+    }
+
+    public void setSdbConfPath(String sdbConfPath) {
+        this.sdbConfPath = sdbConfPath;
+    }
+
     private void init(){
-        sdbPath = "/Users/Yifan/sdb/dev/securedb/target/conf";
-        initClientDbConf(sdbPath +  "/sdb-client.xml");
-        initServerDbConf(sdbPath + "/sdb-server.xml");
-        initMetaDbConf(sdbPath + "/sdb-metastore.xml");
-        initConnectionConf(sdbPath + "/sdb-connection.xml");
+        setSdbConfPath(loadSystemSdbPath());
+        initClientDbConf(sdbConfPath + "/" + CLIENT_CONF_FILE);
+        initServerDbConf(sdbConfPath  + "/" + SERVER_CONF_FILE);
+        initMetaDbConf(sdbConfPath  + "/" + METASTORE_CONF_FILE);
+        initConnectionConf(sdbConfPath  + "/" + CONNECTION_CONF_FILE);
     }
 
     private void initClientDbConf(String filename){
@@ -76,16 +91,22 @@ public class SdbConf {
     }
 
     private void initServerDbConf(String filename){
-        serverDbConf = DbConfFactory.getDbConf(sdbPath +  "/sdb-server.xml");
+        serverDbConf = DbConfFactory.getDbConf(filename);
     }
 
+    //TODO: config metastore for hive.
     private void initMetaDbConf(String filename){
-        metaDbConf = DbConfFactory.getDbConf(sdbPath +  "/sdb-metastore.xml");
+        metaDbConf = DbConfFactory.getDbConf(filename);
     }
 
     //TODO: Change to XML format reader
     private void initConnectionConf(String filename){
         return;
+    }
+
+    private String loadSystemSdbPath(){
+        Map<String, String> env = System.getenv();
+        return env.get(SDB_CONF);
     }
 
 }
---
+++ src/test/java/edu/hku/sdb/conf/SdbConfTest.java
@@ -0,0 +1,19 @@
+package edu.hku.sdb.conf;
+
+import junit.framework.TestCase;
+
+public class SdbConfTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testGetConnectionConf() throws Exception {
+
+    }
+}
