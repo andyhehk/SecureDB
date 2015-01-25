9ff242d025a20d1197e0c41753a64d37202b4666
Eric Haibin Lin <linhaibin.eric@gmail.com>
2015-01-25 11:53:02 +0800

    + Test connection for metastore db.

--- src/test/java/edu/hku/sdb/conf/MysqlDbConfTest.java
+++ src/test/java/edu/hku/sdb/conf/MysqlDbConfTest.java
@@ -9,17 +9,19 @@ import java.sql.Statement;
 public class MysqlDbConfTest extends TestCase {
 
     DbConf clientDbConf;
+    DbConf metastoreDbConf;
     public void setUp() throws Exception {
         super.setUp();
         DbConfFactory dbConfFactory = new DbConfFactory();
         clientDbConf = dbConfFactory.getDbConf("/Users/Yifan/sdb/dev/securedb/target/conf/sdb-client.xml");
+        metastoreDbConf = dbConfFactory.getDbConf("/Users/Yifan/sdb/dev/securedb/target/conf/sdb-metastore.xml");
     }
 
     public void tearDown() throws Exception {
 
     }
 
-    public void testGetConnection() throws Exception {
+    public void testGetConnectionClient() throws Exception {
         Connection connection = clientDbConf.getConnection();
         assertNotNull(connection);
         Statement statement = connection.createStatement();
@@ -29,6 +31,19 @@ public class MysqlDbConfTest extends TestCase {
         while (resultSet.next()){
             int salary = resultSet.getInt("salary");
             assertNotNull(salary);
+        }
+    }
+
+    public void testGetConnectionMetastore() throws Exception {
+        Connection connection = metastoreDbConf.getConnection();
+        assertNotNull(connection);
+        Statement statement = connection.createStatement();
+        assertNotNull(statement);
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM column_key;");
+        assertNotNull(resultSet);
+        while (resultSet.next()){
+            String tableName = resultSet.getString("table_name");
+            assertNotNull(tableName);
         }
     }
 }
--- src/test/java/edu/hku/sdb/conf/SdbConfTest.java
+++ src/test/java/edu/hku/sdb/conf/SdbConfTest.java
@@ -2,18 +2,21 @@ package edu.hku.sdb.conf;
 
 import junit.framework.TestCase;
 
-public class SdbConfTest extends TestCase {
+import java.util.Map;
 
+public class SdbConfTest extends TestCase {
+    SdbConf sdbConf;
     public void setUp() throws Exception {
         super.setUp();
-
+        sdbConf = new SdbConf();
     }
 
     public void tearDown() throws Exception {
 
     }
 
-    public void testGetConnectionConf() throws Exception {
+    public void testGetSdbPath() throws Exception {
+
 
     }
 }
