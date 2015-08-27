b813315933b4937d510cca9eb4561377140343f5
andy <hochion@gmail.com>
2015-08-27 18:49:15 +0800

    fix compile error

--- proxy/src/main/java/edu/hku/sdb/parse/FunctionCallExpr.java
+++ proxy/src/main/java/edu/hku/sdb/parse/FunctionCallExpr.java
@@ -24,7 +24,6 @@ import edu.hku.sdb.catalog.MetaStore;
 import edu.hku.sdb.catalog.Type;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import sun.org.mozilla.javascript.Function;
 
 import java.util.ArrayList;
 import java.util.List;
